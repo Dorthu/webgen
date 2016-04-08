e8522a9f979cbad26010162024e22112b135f135
Dorthu <dorthu2@gmail.com>
2016-04-07 22:32:45 -0400

    Initial commit - proof of concept

---
+++ loaders/blog_loader.py
@@ -0,0 +1,19 @@
+posts = [
+    {
+        "title": "Post Title",
+        "subtitle": "Post Subtitle",
+        "content": "This is the body of the post woooooooo!",
+        "type": "post",
+        "stamp": 2,
+    },
+    {
+        "title": "Post 2",
+        "subtitle": "I posted another thing!",
+        "content": "This part is markdown?",
+        "type": "post",
+        "stamp": 4
+    },
+]
+
+def load_content():
+    return posts
---
+++ loaders/twitter_loader.py
@@ -0,0 +1,10 @@
+tweets = [
+    {
+        "content": "THis is #words @people",
+        "type":"tweet",
+        "stamp": 3
+    }
+]
+
+def load_content():
+    return tweets
