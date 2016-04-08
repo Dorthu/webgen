6217ede240bdb98d89fc219dc0aa0b8669d7d8f9
Dorthu <dorthu2@gmail.com>
2016-04-08 17:32:31 -0400

    That's how twitter works

--- loaders/twitter_loader.py
+++ loaders/twitter_loader.py
@@ -6,5 +6,14 @@ tweets = [
     }
 ]
 
+import tweepy
+
+t = tweepy.OAuthHandler(twitter_oauth_token, twitter_oauth_secret)
+t.set_access_token(twitter_access_token, twitter_access_secret)
+api = tweepy.API(t)
+
+u = api.get_user('dorthu22')
+
+
 def load_content():
     return tweets
