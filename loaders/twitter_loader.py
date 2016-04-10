719a744ad182e9a29f3e2fe52d6d0d812c6c4cbc
Dorthu <dorthu2@gmail.com>
2016-04-09 22:08:24 -0400

    More refinements

--- loaders/twitter_loader.py
+++ loaders/twitter_loader.py
@@ -10,11 +10,15 @@ def load_content(config):
     ret = []
 
     for t in tweets:
+        if t.text[0] == '@':
+            continue
+
         ret.append({
             "type": "tweet",
             "content": t.text,
             "stamp": t.created_at.timestamp(),
             "created": t.created_at,
+            "tweet_url": "https://twitter.com/{}/status/{}".format(config['user'], t.id),
         })
 
     return ret
